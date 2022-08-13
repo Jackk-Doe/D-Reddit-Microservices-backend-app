52266d44c6ab528abd1592ee83affd4cbf5b5e39
Jackk-Doe <dodo-stv@hotmail.com>
2022-08-13 16:15:18 +1200

    Commit the (forgotten) services file

--- post-services/app/services.py
+++ post-services/app/services.py
@@ -6,18 +6,27 @@ import schemas as _schemas
 import models as _models
 
 
-'''
+"""
 Room Routes 
-'''
+"""
 
 
 async def get_rooms(db: Session):
     return [_schemas.Room.from_orm(_rooms) for _rooms in db.query(_models.Room).all()]
 
+async def get_room_by_id(id: int, db: Session):
+    _room = db.query(_models.Room).get(id)
+    if not _room:
+        raise HTTPException(status_code=404, detail="Given room id not found")
+    return _schemas.Room.from_orm(_room)
 
-async def create_room(*, room: _schemas.RoomCreate, db: Session):
+
+async def create_room(room: _schemas.RoomCreate, db: Session):
 
     async def check_topics_exist(topic: str):
+        '''
+        If existed return, else create new Topic
+        '''
         topic_search: _models.Topic = db.query(
             _models.Topic).filter_by(topic_name=topic).first()
         if topic_search:
@@ -38,40 +47,58 @@ async def create_room(*, room: _schemas.RoomCreate, db: Session):
     return _schemas.Room.from_orm(_room)
 
 
-'''
+async def delete_room(id: int, db: Session):
+    _room = db.query(_models.Room).get(id)
+    if not _room:
+        raise HTTPException(
+            status_code=404, detail="The given room id not found")
+    db.delete(_room)
+    db.commit()
+
+
+async def add_message_to_room(room_id: int, message: _schemas.MessageCreate, db: Session):
+    _room = db.query(_models.Room).get(room_id)
+    if not _room:
+        raise HTTPException(status_code=404, detail="The given room id not found")
+    _message = _models.Message(body=message.body, user_id=message.user_id, room_id=room_id)
+    db.add(_message)
+    db.commit()
+    db.refresh(_message)
+    return _schemas.Room.from_orm(_room)
+
+"""
 Messages Routes 
-'''
+"""
 
 
 async def get_messages(db: Session):
     return [_schemas.Message.from_orm(_messages) for _messages in db.query(_models.Message).all()]
 
 
-async def create_message(message: _schemas.MessageCreate, db: Session):
-    try:
-        _message = _models.Message(**message.dict())
-        db.add(_message)
-        db.commit()
-        db.refresh(_message)
-        return _schemas.Message.from_orm(_message)
-    except:
-        db.rollback()
-        raise HTTPException(status_code=404, detail="Fail to add new Message")
+# async def create_message(message: _schemas.MessageCreate, db: Session):
+#     try:
+#         _message = _models.Message(**message.dict())
+#         db.add(_message)
+#         db.commit()
+#         db.refresh(_message)
+#         return _schemas.Message.from_orm(_message)
+#     except:
+#         db.rollback()
+#         raise HTTPException(status_code=404, detail="Fail to add new Message")
 
 
 async def delete_message(message_id: int, db: Session):
     _message = db.query(_models.Message).get(message_id)
-    if _message:
-        db.delete(_message)
-        db.commit()
-    else:
+    if not _message:
         raise HTTPException(
             status_code=404, detail="Given message id for delete not found")
+    db.delete(_message)
+    db.commit()
 
 
-'''
+"""
 Topics Routes 
-'''
+"""
 
 
 async def get_topics(db: Session):
