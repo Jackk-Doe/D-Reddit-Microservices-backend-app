01610fed6b038eb646aaf383361ea0454fd07548
Jackk-Doe <dodo-stv@hotmail.com>
2022-08-16 14:45:04 +1200

    Install jsonwebtoken pkg & formatted codes

--- user-services/index.js
+++ user-services/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');
 
 const router = require('./routes');
 const mongodbConnection = require('./database');
@@ -7,13 +8,16 @@ const mongodbConnection = require('./database');
 
 const app = express();
 
+// .env 
 dotenv.config();
+const PORT = process.env.PORT || 5000;
+const CONNECTION_URL = process.env.CONNECTION_URL;
 
+// Loaders
+app.use(cors());
 app.use(express.json());
 app.use('/users', router);
 
-const PORT = process.env.PORT || 5000;
-const CONNECTION_URL = process.env.CONNECTION_URL;
 
 await mongodbConnection(CONNECTION_URL);
 
