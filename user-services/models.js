e0ee82d8d11e17e95ec89eae3aa5797baacd9f05
Jackk-Doe <dodo-stv@hotmail.com>
2022-08-16 14:37:04 +1200

    Update user model file

--- user-services/models.js
+++ user-services/models.js
@@ -1,1 +1,24 @@
-// TODO : Create mongoose models
+const mongoose = require('mongoose');
+
+const UserSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    bio: {
+        type: String,
+        required: true
+    }
+});
+
+const UserModel = mongoose.model('user', UserSchema);
+
+module.exports = UserModel;
