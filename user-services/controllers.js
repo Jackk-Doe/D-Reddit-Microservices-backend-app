0117d8767939b93580843d6f0630102fa061cfcd
Jackk-Doe <dodo-stv@hotmail.com>
2022-08-16 17:06:27 +1200

    Set up basic Routes and Controllers

--- user-services/controllers.js
+++ user-services/controllers.js
@@ -1,3 +1,15 @@
-// TODO : Create controllers
+const UserModel = require('./models');
 
-class UserController {}
+class UserController {
+    static async getUser(req, res) {
+        UserModel.findById(req.params.id)
+            .then(user => res.status(200).json(user))
+            .catch(error => res.status(404).json({ error: 'User not found'}));
+    }
+
+    static async signIn(req, res) {}
+
+    static async signUp(req, res) {}
+}
+
+module.exports = UserController;
--- user-services/routes.js
+++ user-services/routes.js
@@ -1,7 +1,14 @@
 const express = require('express');
 
+const UserController = require('./controllers');
+
 const router = express.Router();
 
-// TODO : Create routes
+router.get('/test', async (req, res) => res.json({ message: 'Testing API Route!' }));
+
+router.get('/:id', UserController.getUser);
+
+router.post('/signup', UserController.signIn);
+router.post('/signin', UserController.signUp);
 
 module.exports = router;
