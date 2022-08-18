f5541e372fc951e1062cdcbbf15cf5ca6a0eed8f
Jackk-Doe <dodo-stv@hotmail.com>
2022-08-18 17:46:36 +1200

    Implemented LogIn system

--- user-services/controllers.js
+++ user-services/controllers.js
@@ -6,17 +6,23 @@ const UserModel = require("./models");
 class UserController {
   static async getUser(req, res) {
     try {
-        const user = await UserModel.findById(req.params.id);
+      const user = await UserModel.findById(req.params.id);
 
-        if (!user) {
-            res.status(404).json({ detail: "User not found"});
-        }
+      if (!user) {
+        res.status(404).json({ detail: "User not found" });
+      }
 
-        const userRead = { name: user.name, id: user._id, email: user.email, role: user.role, bio: user.bio };
+      const userRead = {
+        name: user.name,
+        id: user._id,
+        email: user.email,
+        role: user.role,
+        bio: user.bio,
+      };
 
-        res.status(200).json({ user: userRead });
+      res.status(200).json({ user: userRead });
     } catch (error) {
-        res.status(500).json({ detail: "Can't get User", error });
+      res.status(500).json({ detail: "Can't get User" });
     }
   }
 
@@ -51,14 +57,38 @@ class UserController {
       );
 
       res.status(201).json({ user, token });
-
     } catch (error) {
-      res.status(500).json({ detail: "Can't create account", error });
+      res.status(500).json({ detail: "Can't create account" });
     }
   }
 
   /// POST [ /users/signin ]
-  static async signIn(req, res) {}
+  static async signIn(req, res) {
+    const { email, password } = req.body;
+    try {
+      const user = await UserModel.findOne({ email });
+      // Check if User existed
+      if (!user) {
+        return res.status(404).json({ detail: "User not found" });
+      }
+
+      // Check User inputed password
+      const isPasswordCorrect = await bcrypt.compare(password, user.password);
+      if (!isPasswordCorrect) {
+        return res.status(404).json({ detail: "Wrong password" });
+      }
+
+      // Generate JWT token
+      const token = jwt.sign(
+        { email: user.email, id: user._id },
+        process.env.SECRET_KEY
+      );
+
+      res.status(200).json({ user, token });
+    } catch (error) {
+      res.status(500).json({ detail: "Can't login an account", error });
+    }
+  }
 }
 
 module.exports = UserController;
