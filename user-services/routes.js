97bed4f826ff453bf117291e2c1ccfea26386fde
Jackk-Doe <dodo-stv@hotmail.com>
2022-08-22 15:27:34 +1200

    Add getAll route in users-services (FOR TESTING)

--- user-services/routes.js
+++ user-services/routes.js
@@ -6,6 +6,9 @@ const router = express.Router();
 
 router.get('/test', async (req, res) => res.json({ message: 'Testing API Route!' }));
 
+/// FOR TESTING ONLY
+router.get('/', UserController.getAll);
+
 router.get('/:id', UserController.getUser);
 
 router.post('/signup', UserController.signUp);
