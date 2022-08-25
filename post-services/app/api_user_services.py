2a69ab585d98df6ab3cd07de868c3b8d3fb198ab
Jackk-Doe <dodo-stv@hotmail.com>
2022-08-25 21:37:49 +1200

    Created services to send req to User-services API

--- post-services/app/api_user_services.py
+++ post-services/app/api_user_services.py
@@ -1,27 +1,28 @@
-from fastapi import HTTPException
+from fastapi import HTTPException, Depends
+from fastapi.security import OAuth2PasswordBearer
 import httpx
 
 import load_envs as _envs
 
+# _oauth2schema = OAuth2PasswordBearer("/api/token")
+_oauth2schema = OAuth2PasswordBearer(tokenUrl="token")
 
-async def validate_user(user_id: str):
+async def validate_token(token: str = Depends(_oauth2schema)):
+    '''
+    Validate User account through a given [token] with User-services, then return [user_id]
+    '''
     try:
-        res = httpx.get(f"{_envs.USER_SERVICES_URL}/{user_id}")
+        _headers = {'Authorization': 'Bearer ' + token}
+        res = httpx.post(f"{_envs.USER_SERVICES_URL}/token", headers=_headers)
     except:
-        raise Exception("User-services API Error")
+        raise HTTPException(status_code=400, detail="No response from User API")
+
+    # [res_datas] contains either : 'user_id' (SUCCESS) | 'detail' (FAIL)
+    _res_datas = res.json()
+
     if res.status_code != 200:
         #! Error : Connection successed, but no User found
-        _datas = res.json()
-        _detail = _datas['detail'] if _datas['detail'] != None else "No response from User API"
-        raise Exception(_detail)
+        _error_detail = _res_datas['detail']
+        raise HTTPException(status_code=res.status_code, detail=_error_detail)
 
-
-async def validate_and_get_user(user_id: str):
-    try:
-        res = httpx.get(f"{_envs.USER_SERVICES_URL}/{user_id}")
-        if res.status_code != 200:
-            return None
-        user = res.text['user']
-        return user
-    except:
-        raise Exception("API exception")
+    return _res_datas['user_id']
