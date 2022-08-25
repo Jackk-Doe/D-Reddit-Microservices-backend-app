86073f342f4272a38cfd04df6dc3957d864018bf
Jackk-Doe <dodo-stv@hotmail.com>
2022-08-25 18:05:25 +1200

    Added user-token check func, in User-services

--- user-services/controllers.js
+++ user-services/controllers.js
@@ -17,23 +17,48 @@ class UserController {
   static async getUser(req, res) {
     try {
       // If a given [id] field is not in 24 hex character, it throws error (mongoose)
-      const user = await UserModel.findById(req.params.id);
+      const existedUser = await UserModel.findById(req.params.id);
 
-      if (!user) {
+      if (!existedUser) {
+        //! Error : User not existed
         return res.status(404).json({ detail: "User not found" });
       }
 
       const userRead = {
-        name: user.name,
-        id: user._id,
-        email: user.email,
-        role: user.role,
-        bio: user.bio,
+        name: existedUser.name,
+        id: existedUser._id,
+        email: existedUser.email,
+        role: existedUser.role,
+        bio: existedUser.bio,
       };
 
       res.status(200).json({ user: userRead });
     } catch (error) {
       res.status(500).json({ detail: "Error, can not get User" });
+    }
+  }
+
+  /// POST [ /users/token ]
+  static async validateUserToken(req, res) {
+    try {
+      // Get TOKEN
+      const token = req.headers.authorization.split(" ")[1];
+
+      /// Decode ID from the token
+      /// [decodedDatas] contains [email], [id] & [iat] fields
+      const decodedDatas = jwt.decode(token, process.env.SECRET_KEY);
+
+      const _user_id = decodedDatas?.id;
+
+      const existedUser = await UserModel.findById(_user_id);
+      if (!existedUser) {
+        //! Error : User not existed
+        return res.status(404).json({ detail: "User not found" });
+      }
+
+      res.status(200).json({ user_id: _user_id });
+    } catch (error) {
+      res.status(500).json({ detail: "Error, problem with token decoding", error})
     }
   }
 
@@ -61,7 +86,8 @@ class UserController {
         bio,
       });
 
-      // Generate JWT token
+      /// Create JWT token,
+      /// from User's email + User's ID
       const token = jwt.sign(
         { email: user.email, id: user._id },
         process.env.SECRET_KEY
@@ -86,10 +112,12 @@ class UserController {
       // Check User inputed password
       const isPasswordCorrect = await bcrypt.compare(password, user.password);
       if (!isPasswordCorrect) {
+        //! Error : Wrong password
         return res.status(404).json({ detail: "Wrong password" });
       }
 
-      // Generate JWT token
+      /// Receive JWT token,
+      /// from User's email + User's ID
       const token = jwt.sign(
         { email: user.email, id: user._id },
         process.env.SECRET_KEY
--- user-services/routes.js
+++ user-services/routes.js
@@ -6,10 +6,12 @@ const router = express.Router();
 
 router.get('/test', async (req, res) => res.json({ message: 'Testing API Route!' }));
 
-/// FOR TESTING ONLY
+/// For ADMIN uses
 router.get('/', UserController.getAll);
-
 router.get('/:id', UserController.getUser);
+
+/// Post-services mostly uses only these route(s)
+router.post('/token', UserController.validateUserToken);
 
 router.post('/signup', UserController.signUp);
 router.post('/signin', UserController.signIn);
