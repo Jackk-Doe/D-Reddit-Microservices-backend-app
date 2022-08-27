682716317eb32a5585fb61169b302bf81c9d931f
Jackk-Doe <dodo-stv@hotmail.com>
2022-08-27 22:28:16 +1200

    Created Add-Message function

--- client API gateway/app/routes/posts.py
+++ client API gateway/app/routes/posts.py
@@ -85,3 +85,16 @@ async def deleteRoom(room_id: int, token: str = Depends(oauth2_scheme)):
         return _res
     except Exception as _error:
         return HTTPException(status_code=500, detail=str(_error))
+
+
+@router.post('/rooms/{room_id}/add-message')
+async def addMessage(room_id: int, payload: dict = Body(), token: str = Depends(oauth2_scheme)):
+    try:
+        _headers = {'Authorization': 'Bearer ' + token}
+        res = httpx.post(f"{_envs.POST_SERVICES_URL}/posts/rooms/{room_id}/add-message", json=payload, headers=_headers)
+        _res = res.json()
+        if res.status_code != 200:
+            return {'status_code': res.status_code, 'detail': _res['detail']}
+        return _res
+    except Exception as _error:
+        return HTTPException(status_code=500, detail=str(_error))
