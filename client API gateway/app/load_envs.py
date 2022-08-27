f408712fe6239593416920b88b70c2487be93d95
Jackk-Doe <dodo-stv@hotmail.com>
2022-08-27 16:46:15 +1200

    Success connection test with Post-services

--- client API gateway/app/load_envs.py
+++ client API gateway/app/load_envs.py
@@ -4,3 +4,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 PORT = getenv('PORT')
+POST_SERVICES_URL = getenv('POST_SERVICES_URL')
+USER_SERVICES_URL = getenv('USER_SERVICES_URL')
--- client API gateway/app/routes/posts.py
+++ client API gateway/app/routes/posts.py
@@ -1,7 +1,18 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+
+import load_envs as _envs
 
 router = APIRouter()
 
 @router.get('')
 async def testRoute():
     return {"TEST":"Hello from POST route"}
+
+@router.get('/test')
+async def testConnect():
+    import httpx
+    try:
+        res = httpx.get(f"{_envs.POST_SERVICES_URL}")
+        return res.json()
+    except Exception as _error:
+        return HTTPException(status_code=500, detail=str(_error))
