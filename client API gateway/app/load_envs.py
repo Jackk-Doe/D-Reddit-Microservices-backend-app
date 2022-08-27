302ec3f9981b44ce89b7996c89f246ca0f59ec87
Jackk-Doe <dodo-stv@hotmail.com>
2022-08-27 16:03:14 +1200

    Set up .env and its related functions

---
+++ client API gateway/app/load_envs.py
@@ -0,0 +1,6 @@
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+
+PORT = getenv('PORT')
--- client API gateway/app/main.py
+++ client API gateway/app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 
+import load_envs as _envs
+
 app = FastAPI()
 
 @app.get('/')
@@ -9,6 +11,7 @@ async def home():
 if __name__ == '__main__':
     import uvicorn
 
-    PORT = 3000
+    PORT = int(_envs.PORT)
 
+    # NOTE : Need to specify [host] param in uvicorn.run(), else Not getting any reply from server, when in Docker
     uvicorn.run("main:app", host='0.0.0.0', port=PORT, reload=True)
