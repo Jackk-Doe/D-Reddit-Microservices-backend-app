b835af0da8edef2e0d079c1fd5cf3e75b1a99d0f
Jackk-Doe <dodo-stv@hotmail.com>
2022-08-27 17:12:32 +1200

    Implemented GetById + formatted codes

--- client API gateway/app/routes/posts.py
+++ client API gateway/app/routes/posts.py
@@ -24,10 +24,23 @@ async def testConnect():
 async def getRooms():
     try:
         res = httpx.get(f"{_envs.POST_SERVICES_URL}/posts/rooms")
-        _res_datas = res.json()
+        _res = res.json()
         if res.status_code is not 200:
             #! Error : Found error from calling service
-            return {'status_code': res.status_code, 'detail': _res_datas['detail']}
-        return _res_datas
+            return {'status_code': res.status_code, 'detail': _res['detail']}
+        return _res
     except Exception as _error:
         return HTTPException(status_code=500, detail=_error)
+
+
+@router.get('/rooms/{room_id}')
+async def getById(room_id: int):
+    try:
+        res = httpx.get(f"{_envs.POST_SERVICES_URL}/posts/rooms/{room_id}")
+        _res = res.json()
+        if res.status_code is not 200:
+            #! Error
+            return {'status_code': res.status_code, 'detail': _res['detail']}
+        return _res
+    except Exception as _error:
+        return HTTPException(status_code=500, detail=str(_error))
