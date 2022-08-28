4f64ecb74e8c61ca15a8b94c4dd6555b97352795
Jackk-Doe <dodo-stv@hotmail.com>
2022-08-28 17:07:48 +1200

    route to test connection of Gateway and User-serv

--- client API gateway/app/routes/users.py
+++ client API gateway/app/routes/users.py
@@ -1,7 +1,19 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+import httpx
+
+import load_envs as _envs
 
 router = APIRouter()
 
 @router.get('')
 async def testRoute():
     return {"TEST":"Hello from USER route"}
+
+
+@router.get('/test')
+async def testConnection():
+    try:
+        res = httpx.get(f"{_envs.USER_SERVICES_URL}/")
+        return res.json()
+    except Exception as _error:
+        return HTTPException(status_code=500, detail=str(_error))
