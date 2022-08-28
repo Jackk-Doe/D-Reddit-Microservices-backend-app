c4cd064ff73751c9f4caaab2b0ec5c1a3aad589c
Jackk-Doe <dodo-stv@hotmail.com>
2022-08-28 17:19:02 +1200

    Created SignUp route in Gateway

--- client API gateway/app/routes/users.py
+++ client API gateway/app/routes/users.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Body
 import httpx
 
 import load_envs as _envs
@@ -7,7 +7,7 @@ router = APIRouter()
 
 @router.get('')
 async def testRoute():
-    return {"TEST":"Hello from USER route"}
+    return {"TEST":"Hello from APIGateway : USER route"}
 
 
 @router.get('/test')
@@ -15,5 +15,17 @@ async def testConnection():
     try:
         res = httpx.get(f"{_envs.USER_SERVICES_URL}/")
         return res.json()
+    except Exception as _error:
+        return HTTPException(status_code=500, detail=str(_error))
+
+
+@router.post('/signup')
+async def signupUser(payload: dict = Body()):
+    try:
+        res = httpx.post(f"{_envs.USER_SERVICES_URL}/users/signup", json=payload)
+        _res = res.json()
+        if res.status_code != 201:
+            return {'status_code': res.status_code, 'detail': _res['detail']}
+        return _res
     except Exception as _error:
         return HTTPException(status_code=500, detail=str(_error))
