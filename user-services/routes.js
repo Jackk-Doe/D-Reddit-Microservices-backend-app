0e83dad60bbbdd3110566d7466a2438636b5bc07
Jackk-Doe <dodo-stv@hotmail.com>
2022-09-24 16:54:45 +1200

    Update : get User datas routes

--- user-services/routes.js
+++ user-services/routes.js
@@ -6,8 +6,8 @@ const router = express.Router();
 
 
 /// For ADMIN uses  (or Testing)
-router.get('/', UserController.getAll);
-router.get('/:id', UserController.getUser);
+router.get('/account', UserController.getAll);
+router.get('/account/:id', UserController.getUser);
 
 /// Post-services mostly uses only these route(s)
 router.post('/token', UserController.validateUserToken);
@@ -15,7 +15,7 @@ router.patch('/views', UserController.updateView);
 
 // TODO : a route to return User [views] 
 
-/// For API-Gateway uses
+/// For API-Gateway uses   (DIRECT uses)
 router.post('/signup', UserController.signUp);
 router.post('/signin', UserController.signIn);
 
