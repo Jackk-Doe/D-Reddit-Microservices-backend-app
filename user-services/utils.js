878740854ebc15bd3103daa9d10e3d6f7f105e05
Jackk-Doe <dodo-stv@hotmail.com>
2022-09-24 15:20:46 +1200

    Update : improved Token & UserID read function
    - Updated function that check Auth-Token or UserID (req.body) in UserServices Utils.js

--- user-services/utils.js
+++ user-services/utils.js
@@ -2,19 +2,24 @@ const TokenAuthServices = require("./tokenAuthServices");
 
 class Utils {
 
-    /// Return UserID : generates from Token or Request body
+    /// Return UserID : generates from Header-Auth-Token or Request-body
     static async check_id_from_token_or_param_body(req) {
-        const token = req.headers.authorization.split(" ")[1];
-        const decodedDatas = await TokenAuthServices.decodeToken(token);
 
-        if (decodedDatas) {
-            return decodedDatas.id;
+        /// Via Auth-Token : update User.views
+        if (req.headers.authorization) {
+            console.log("FOUND HEADER AUTH Token, with Request");
+            const token = req.headers.authorization.split(" ")[1];
+            const decodedDatas = await TokenAuthServices.decodeToken(token);
+    
+            if (decodedDatas) {
+                return decodedDatas.id;
+            }
         }
 
-        const { id } = req.body;
-
-        if (id) {
-            return id
+        /// Via UserID : update User.views
+        if (req.body.id) {
+            const { id } = req.body;
+            return id;
         }
 
         return null;
