02a0b57b4119b2522e8890b1896cae364baa27a2
Jackk-Doe <dodo-stv@hotmail.com>
2022-09-29 18:05:53 +1300

    Update : comments in posts.py of APIGateway

--- client-API-gateway/app/routes/posts.py
+++ client-API-gateway/app/routes/posts.py
@@ -12,11 +12,17 @@ token_auth_scheme = HTTPBearer(auto_error=False)
 
 @router.get('/')
 async def testRoute():
+    '''
+    Test POST-related routes of the custom API Gateway
+    '''
     return {"TEST": "Hello from APIGateway : POST route"}
 
 
 @router.get('/test')
 async def testConnect():
+    '''
+    Test API connection, by sending request to the POST Services
+    '''
     try:
         res = httpx.get(f"{_envs.POST_SERVICES_URL}")
         return res.json()
@@ -26,6 +32,10 @@ async def testConnect():
 
 @router.get('/rooms')
 async def getRooms(*, recommend: bool = False, token: str = Depends(token_auth_scheme)):
+    '''
+    Get all Rooms. with avialable query string parameters : 
+    - **recommend**: to get backend generating Recommend Rooms, generate from User's interested (viewed) topics, **Bearer Token** is required
+    '''
     try:
         if recommend:
             if token is None:
@@ -47,6 +57,12 @@ async def getRooms(*, recommend: bool = False, token: str = Depends(token_auth_s
 
 @router.get('/rooms/{room_id}')
 async def getById(room_id: int, token: str = Depends(token_auth_scheme)):
+    '''
+    Get a Room by its ID,
+
+    if passing **Bearer token** with this request, record this Room topics into User' view history
+    \f
+    '''
     try:
         if token is not None:
             _headers = {'Authorization': f"Bearer {token.credentials}"}
@@ -64,6 +80,18 @@ async def getById(room_id: int, token: str = Depends(token_auth_scheme)):
 
 @router.post('/rooms')
 async def createRoom(payload: dict = Body(example={"room_name": "Room name", "body": "Room body (main text)", "topics": ["beginner", "python"]}), token: str = Depends(token_auth_scheme)):
+    """
+    Create a Room with all the information:
+
+    - **room_name**: a name of this new Room
+    - **body**: a long description
+    - **topics**: a list of topics about this Room
+
+    - **Bearer token for authenication** 
+
+    Also update User's interested topics, from topics of this Room
+    \f
+    """
     try:
         if token is None:
             return HTTPException(status_code=401, detail="Token is required")
@@ -80,6 +108,22 @@ async def createRoom(payload: dict = Body(example={"room_name": "Room name", "bo
 
 @router.patch('/rooms/{room_id}')
 async def updateRoom(room_id: int, payload: dict = Body(example={"room_name": "New Room name", "body": "New Room body (main text)", "topics": ["beginner", "python"]}), token: str = Depends(token_auth_scheme)):
+    """
+    Update a Room with all the information:
+
+    NOTE : Only a Room owner can update a person's Room
+
+    - **room_id**: an ID of a update Room
+
+    - **room_name**: an updating name of this new Room
+    - **body**: an updating long description
+    - **topics**: an updating list of topics about this Room
+
+    - **Bearer token for authenication** 
+
+    Also update User's interested topics, from topics of this Room (if topics changed)
+    \f
+    """
     try:
         if token is None:
             return HTTPException(status_code=401, detail="Token is required")
@@ -96,6 +140,14 @@ async def updateRoom(room_id: int, payload: dict = Body(example={"room_name": "N
 
 @router.delete('/room/{room_id}')
 async def deleteRoom(room_id: int, token: str = Depends(token_auth_scheme)):
+    '''
+    Delete a Room by its ID,
+
+    - **Bearer token for authenication** 
+
+    NOTE : Only a Room owner can delete a person's Room
+    \f
+    '''
     try:
         if token is None:
             return HTTPException(status_code=401, detail="Token is required")
@@ -112,6 +164,16 @@ async def deleteRoom(room_id: int, token: str = Depends(token_auth_scheme)):
 
 @router.post('/rooms/{room_id}/add-message')
 async def addMessage(room_id: int, payload: dict = Body(example={"body": "This is a comment message to add into this Room"}), token: str = Depends(token_auth_scheme)):
+    """
+    Add comment (messages) to a Room
+
+    NOTE : User must has an account to add (post) comment 
+
+    - **room_id**: an ID of a update Room
+    - **Bearer token for User account validation** 
+
+    \f
+    """
     try:
         if token is None:
             return HTTPException(status_code=401, detail="Token is required")
