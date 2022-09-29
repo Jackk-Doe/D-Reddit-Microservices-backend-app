2eca087ecca5e2cee984dc6ddeccba0a67803966
Jackk-Doe <dodo-stv@hotmail.com>
2022-09-29 17:20:24 +1300

    Update : comments in APIGateway users.py

--- client-API-gateway/app/routes/users.py
+++ client-API-gateway/app/routes/users.py
@@ -7,11 +7,17 @@ router = APIRouter()
 
 @router.get('/')
 async def testRoute():
+    '''
+    Test User-related routes of the custom API Gateway
+    '''
     return {"TEST":"Hello from APIGateway : USER route"}
 
 
 @router.get('/test')
 async def testConnection():
+    '''
+    Test API connection, by sending request to the USER Services
+    '''
     try:
         res = httpx.get(f"{_envs.USER_SERVICES_URL}/")
         return res.json()
@@ -21,6 +27,10 @@ async def testConnection():
 
 @router.post('/signup')
 async def signupUser(payload: dict = Body(example={"name": "User name (e.g: Jackk Doe)", "email": "User email here", "password": "User password here", "bio": "About User bio"})):
+    '''
+    Sign up with [name], [email], [password], [bio] to create User account,
+    to receive User account datas and [token] from User-Services
+    '''
     try:
         res = httpx.post(f"{_envs.USER_SERVICES_URL}/users/signup", json=payload)
         _res = res.json()
@@ -33,6 +43,10 @@ async def signupUser(payload: dict = Body(example={"name": "User name (e.g: Jack
 
 @router.post('/signin')
 async def signinUser(payload: dict = Body(example={"email": "User email here", "password": "User password here"})):
+    '''
+    Sign in with [email] & [password] to recieve User account datas,
+    thus receive [name], [email], [bio], & [token] from User-Services
+    '''
     try:
         res = httpx.post(f"{_envs.USER_SERVICES_URL}/users/signin", json=payload)
         _res = res.json()
